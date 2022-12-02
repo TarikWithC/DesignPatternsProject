ea37bee3b4054dd3b53957ec4f9f946b664462b0
Tarik Cabukoglu <tarikcabukoglu@outlook.com>
2022-12-02 19:29:13 +0300

    Pattern Registry Improved

--- Console/ConsoleMenuMessages.cs
+++ Console/ConsoleMenuMessages.cs
@@ -5,7 +5,8 @@ namespace ConsoleApp
     {
         internal const string MainInfo = "\n*Press 0 to close this program.*\n" +
                                          "Current Design Patterns Are\n" +
-                                         "Singleton: 1";
+                                         "Singleton: 1\n" +
+                                         "Lazy Singleton: 2";
 
         internal const string InvalidValue = "Please enter a valid value.";
     }
