9e7173dd4c801e49372826e5a294c83db5932dbf
Tarik Cabukoglu <tarikcabukoglu@outlook.com>
2022-12-04 22:46:46 +0300

    Registeration System Updated

--- Console/PatternTestRegistry.cs
+++ Console/PatternTestRegistry.cs
@@ -1,33 +1,26 @@
 ﻿using System.Text;
 using DesignPatterns;
-using DesignPatterns.CreationalPatterns.SingletonPatterns.DotNetLazySingleton;
-using DesignPatterns.CreationalPatterns.SingletonPatterns.LazySingleton;
-using DesignPatterns.CreationalPatterns.SingletonPatterns.NestedLazySingleton;
-using DesignPatterns.CreationalPatterns.SingletonPatterns.Singleton;
-using DesignPatterns.CreationalPatterns.SingletonPatterns.ThreadSafeLazySingleton;
+using DesignPatterns.CreationalPatterns.SingletonPatterns;
+using DesignPatterns.CreationalPatterns.SingletonPatterns.Initiliazers;
 
 namespace ConsoleApp
 {
     public class PatternTestRegistry
     {
         private readonly int _serialNumber;
-        public Dictionary<int, IPatternTester> Tests = new();
+        public Dictionary<int, IPatternExecutor> Executors = new();
         public StringBuilder PatternNames = new();
 
         public PatternTestRegistry()
         {
-            Tests.TryAdd(++_serialNumber, new SingletonTest());
-            Tests.TryAdd(++_serialNumber, new LazySingletonTest());
-            Tests.TryAdd(++_serialNumber, new ThreadSafeLazySingletonTest());
-            Tests.TryAdd(++_serialNumber, new NestedLazySingletonTest());
-            Tests.TryAdd(++_serialNumber, new DotNetLazySingletonTest());
+            Executors.TryAdd(++_serialNumber, new SingletonExecutor());
 
             SavePatternNames();
         }
 
         public void SavePatternNames()
         {
-            foreach (var item in Tests)
+            foreach (var item in Executors)
             {
                 PatternNames.AppendLine($"{item.Key}: {item.Value.GetPatternName()}");
             }
