e6c08b14d28e8f532f9f928367d6bf0cef25ec71
Tarik Cabukoglu <tarikcabukoglu@outlook.com>
2022-12-04 20:47:53 +0300

    DotNet Lazy Singleton Created

---
+++ DesignPatterns/CreationalPatterns/SingletonPatterns/DotNetLazySingleton/DotNetLazySingleton.cs
@@ -0,0 +1,27 @@
+﻿
+namespace DesignPatterns.CreationalPatterns.SingletonPatterns.DotNetLazySingleton
+{
+    public sealed class DotNetLazySingleton
+    {
+        private static readonly Lazy<DotNetLazySingleton> Instance = new(() => new DotNetLazySingleton());
+        private readonly int _objectNumber;
+        private readonly string _objectName;
+
+        private DotNetLazySingleton()
+        {
+            _objectNumber++;
+            _objectName = $"Instance SN: {_objectNumber}.";
+        }
+
+        //Best practice for thread safe lazy singleton objects using .NET
+        public static DotNetLazySingleton GetObject()
+        {
+            return Instance.Value;
+        }
+
+        public string GetName()
+        {
+            return _objectName;
+        }
+    }
+}
---
+++ DesignPatterns/CreationalPatterns/SingletonPatterns/DotNetLazySingleton/DotNetLazySingletonTest.cs
@@ -0,0 +1,65 @@
+﻿using Core;
+
+namespace DesignPatterns.CreationalPatterns.SingletonPatterns.DotNetLazySingleton
+{
+    public sealed class DotNetLazySingletonTest: IPatternTester
+    {
+        private const string PatternName = "DotNet Lazy Singleton";
+        public string GetPatternName()
+        {
+            return PatternName;
+        }
+
+        private static bool _isTestPassed = true;
+        private const string ExpectedInstanceName = "Instance SN: 1.";
+
+        public void Test()
+        {
+            Console.WriteLine(DotNetLazySingletonConsoleMessages.Information);
+            var input = ConsoleExtension.ReadIntegerFromConsole();
+            switch (input)
+            {
+                case 0:
+                    break;
+                case 1:
+                    StartThreads();
+                    PrintResults();
+                    break;
+                default:
+                    Console.WriteLine(DotNetLazySingletonConsoleMessages.CommandNotFound);
+                    Test();
+                    break;
+            }
+        }
+
+        private static void CreateAndCheckInstance()
+        {
+            var testInstanceName = DotNetLazySingleton.GetObject().GetName();
+            Console.WriteLine(testInstanceName);
+            if (!string.Equals(testInstanceName, ExpectedInstanceName))
+            {
+                _isTestPassed = false;
+            }
+        }
+
+        private static void StartThreads()
+        {
+            Console.WriteLine(DotNetLazySingletonConsoleMessages.CountQuestion);
+            var objectCountToGenerate = ConsoleExtension.ReadIntegerFromConsole();
+            for (var i = 0; i < objectCountToGenerate; i++)
+            {
+                var thread = new Thread(CreateAndCheckInstance);
+                thread.Start();
+                Console.WriteLine($"\nThread Name: {thread.ManagedThreadId} started.");
+            }
+        }
+
+        private static void PrintResults()
+        {
+            Console.WriteLine(_isTestPassed
+                ? DotNetLazySingletonConsoleMessages.TestSucceeded
+                : DotNetLazySingletonConsoleMessages.TestFailed);
+        }
+        
+    }
+}
