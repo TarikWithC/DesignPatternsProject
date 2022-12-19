5ec005570c58c5bab7f83538d0c137909d949863
Tarik Cabukoglu <tarikcabukoglu@outlook.com>
2022-12-20 02:12:46 +0300

    using fix

--- DesignPatterns/CreationalPatterns/AbstractFactoryPatterns/VehicleCompany/Vehicles/Land/Truck.cs
+++ DesignPatterns/CreationalPatterns/AbstractFactoryPatterns/VehicleCompany/Vehicles/Land/Truck.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DesignPatterns.CreationalPatterns.AbstractFactoryPatterns.VehicleCompany.Abstractions;
+﻿using DesignPatterns.CreationalPatterns.AbstractFactoryPatterns.VehicleCompany.Abstractions;
 
 namespace DesignPatterns.CreationalPatterns.AbstractFactoryPatterns.VehicleCompany.Vehicles.Land
 {
