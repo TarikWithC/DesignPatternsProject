d8ae3b02ef08b5940509b90524fcd62873f2333e
Tarik Cabukoglu <tarikcabukoglu@outlook.com>
2022-12-20 02:08:53 +0300

    class created

---
+++ DesignPatterns/CreationalPatterns/AbstractFactoryPatterns/VehicleCompany/Vehicles/Air/Jet.cs
@@ -0,0 +1,12 @@
+﻿using DesignPatterns.CreationalPatterns.AbstractFactoryPatterns.VehicleCompany.Abstractions;
+
+namespace DesignPatterns.CreationalPatterns.AbstractFactoryPatterns.VehicleCompany.Vehicles.Air
+{
+    public class Jet : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Jet is flying.");
+        }
+    }
+}
---
+++ DesignPatterns/CreationalPatterns/AbstractFactoryPatterns/VehicleCompany/Vehicles/Air/Plane.cs
@@ -0,0 +1,12 @@
+﻿using DesignPatterns.CreationalPatterns.AbstractFactoryPatterns.VehicleCompany.Abstractions;
+
+namespace DesignPatterns.CreationalPatterns.AbstractFactoryPatterns.VehicleCompany.Vehicles.Air
+{
+    public class Plane : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Plane is flying.");
+        }
+    }
+}
