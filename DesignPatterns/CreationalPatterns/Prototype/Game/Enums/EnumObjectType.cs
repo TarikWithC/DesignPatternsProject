e0fbf21ff2b83890a7d7bf36d95dc98f9e537a19
Tarik Cabukoglu <tarikcabukoglu@outlook.com>
2022-12-21 19:34:57 +0300

    Prototype Enums and Constants Init

---
+++ DesignPatterns/CreationalPatterns/Prototype/Game/Enums/EnumObjectType.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns.CreationalPatterns.Prototype.Game.Enums
+{
+    internal enum EnumObjectType
+    {
+        Soldier = 1,
+        Vehicle = 2,
+        Weapon = 3
+    }
+}
---
+++ DesignPatterns/CreationalPatterns/Prototype/Game/Enums/EnumWeaponType.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.CreationalPatterns.Prototype.Game.Enums
+{
+    public enum EnumWeaponType
+    {
+        Unknown,
+        Pistol,
+        SubMachineGun,
+        HeavyMachineGun,
+        SniperRifle
+    }
+}
