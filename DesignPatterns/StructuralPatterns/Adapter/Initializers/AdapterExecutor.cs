9d7c975c2059dcf249f081df85f5981ef7ab734b
Tarik Cabukoglu <tarikcabukoglu@outlook.com>
2022-12-28 23:25:50 +0300

    Adapter Pattern Init

---
+++ DesignPatterns/StructuralPatterns/Adapter/Initializers/AdapterExecutor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using DesignPatterns.Base;
+
+namespace DesignPatterns.StructuralPatterns.Adapter.Initializers
+{
+    public class AdapterExecutor : BaseExecutor, IPatternExecutor
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPatternName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns/StructuralPatterns/Adapter/Initializers/AdapterTester.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+
+namespace DesignPatterns.StructuralPatterns.Adapter.Initializers
+{
+    internal class AdapterTester : IPatternTester
+    {
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
